ae3a93c2ef443ddd5663a84efbbdbf743c6a79b5
Raissa <raissa.feitosa06@gmail.com>
2026-05-27 12:01:08 -0300

    feat: create CategoryRepository interface

---
+++ src/main/java/com/generation/gamestore/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.generation.gamestore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gamestore.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	public List<Category> findAllByNameContainingIgnoreCase(String name);
+}
